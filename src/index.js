dcdb651193d3284e3114b9578d46861da0d183c6
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-12-01 19:01:04 +0200

    installed BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { StrictMode, BrowserRouter } from 'react';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
